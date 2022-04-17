abec900e459eaeb3ec824eadc8bb6cfe3701a5bd
MatricalDefunkt <pramitsmusic@gmail.com>
2022-04-17 20:46:14 +0530

    Make it actually work now (since guild.shards wasnt working)

--- src/chatcommands/gettags.js
+++ src/chatcommands/gettags.js
@@ -1,10 +1,9 @@
 const { MessageEmbed } = require('discord.js')
 const { Tags } = require('../database/database')
-const { BaseGuild } = require('discord.js')
 
 module.exports = {
     data: {
-        name: 'gettags',
+        name: 'gettag',
         ownerOnly: false,
         staffOnly: true,
         adminOnly: false
@@ -17,26 +16,57 @@ module.exports = {
         })
         .setColor("GREEN")
         .setDescription(
-            `Syntax and use of 'newtag' command:\n\`\`\`diff\n+   <Mandatory>\n-   [Optional]\`\`\`\n\`\`\`diff\n+   !!gettags <all / tag-name>\`\`\`\n\`\`\`\nUse:\nThe gettags command replies to the command with either all the tags for PYL or, if given a name, data about that tag.\`\`\``
+            `Syntax and use of 'gettags' command:\n\`\`\`diff\n+   <Mandatory>\n-   [Optional]\`\`\`\n\`\`\`diff\n+   !!gettags <all / tag-name>\`\`\`\n\`\`\`\nUse:\nThe gettags command replies to the command with either all the tags for PYL or, if given a name, data about that tag.\`\`\``
     ),
     async execute (msg, client, args) {
 
-        if (args[0] === 'all') {
+        const permissionHandle = (permissionLevel) => {
+            switch (permissionLevel) {
+                case 0:
+                    return 'Everyone';
+                case 1:
+                    return 'Staff-Only';
+                case 2:
+                    return 'Admin-Only';
+                default:
+                    return 'Permission stored invalidly. Please contact Matrical.'
+            }
+        }
 
-            const tags = Tags.findAll()
-            console.log(tags);
+        if (args[0].toLowerCase() === 'all') {
+
+            const tags = await Tags.findAll()
+            const messageBuilder = [];
+            try {
+                tags.forEach(tag => {
+                    messageBuilder.push(`\`\`\`Name: ${tag.getDataValue('tagName')}\n\nReply: ${tag.getDataValue('tagReply')}\n\nPermission Level: ${permissionHandle(tag.getDataValue('tagPerms'))}\n\`\`\`Author: <@${tag.getDataValue('tagAuthor')}>`)
+                });
+            } catch (e) {
+                console.error(e)
+                msg.reply(e)
+            } finally {
+                const joinedReply = messageBuilder.join(';;//..//;;//\n')
+                if (joinedReply.length >= 2000) {
+                    
+                } else {
+                    const finalReply = await joinedReply.replaceAll(';;//..//;;//', '')
+                    if (!finalReply) return msg.reply({content: `No tags have been created yet. You can begin by typing \`<prefix>newtag\`!`})
+                    msg.reply({content: finalReply, embeds: [
+                        new MessageEmbed()
+                        .setTitle('All PYL tags')
+                        .setDescription(finalReply)
+                    ]})
+                }
+            }
 
         } else {
 
-            console.log(msg.guild.shard)
-
             const tag = await Tags.findOne({where: {tagName: `${args[0]}`}})
             if (!tag) return msg.reply({content: `Tag with name ${args[0]} was not found.`})
-            const author = await msg.guild.members.fetch({id: `${tag.getDataValue('tagAuthor')}`, force: false})
             msg.reply({embeds: [
                 new MessageEmbed()
                 .setTitle(`${tag.getDataValue('tagName')}`)
-                .setDescription(`Name: ${tag.getDataValue('tagName')}\nReply: ${tag.getDataValue('tagReply')}\nAuthor: ${author.tag}`)
+                .setDescription(`Name: ${tag.getDataValue('tagName')}\n\nReply: ${tag.getDataValue('tagReply')}\n\nAuthor: <@${tag.getDataValue('tagAuthor')}>`)
             ]});
 
         }
