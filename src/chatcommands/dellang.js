eb0b4577edcd3d71ecd9c5835e03f4c048eb9eae
MatricalDefunkt <pramitsmusic@gmail.com>
2022-04-18 20:29:46 +0530

    Added a confirmation for dellang

--- src/chatcommands/dellang.js
+++ src/chatcommands/dellang.js
@@ -7,30 +7,30 @@ module.exports = {
     staffOnly: false,
     adminOnly: true,
   },
-    helpEmbed: new MessageEmbed()
-    .setTitle("Use of NewLang")
-    .setAuthor({
-      name: "PYL Bot#9640",
-      iconURL: `https://cdn.discordapp.com/avatars/954655539546173470/4c10aad2d82cdff4dcb05a6c83005739.webp`,
-    })
-    .setColor("GREEN")
-    .setDescription(
-      `Syntax and use of 'newlang' command:\n\`\`\`diff\n+   <Mandatory>\n-   [Optional]\`\`\`\n\`\`\`diff\n+   !!newlang <name of new language>\`\`\`\n\`\`\`\nUse:\nNewLang helps in creating a new language category by making a new category, general chat, no-speaking text, and voice channel.\nOnce you've run the first command, that is !!newlang <name of language>, you will be asked for 3 inputs, namely the names of the three channels.\`\`\``
-    ),
-    async execute(msg, client, args) {
+  helpEmbed: new MessageEmbed()
+  .setTitle("Use of NewLang")
+  .setAuthor({
+    name: "PYL Bot#9640",
+    iconURL: `https://cdn.discordapp.com/avatars/954655539546173470/4c10aad2d82cdff4dcb05a6c83005739.webp`,
+  })
+  .setColor("GREEN")
+  .setDescription(
+    `Syntax and use of 'newlang' command:\n\`\`\`diff\n+   <Mandatory>\n-   [Optional]\`\`\`\n\`\`\`diff\n+   !!newlang <name of new language>\`\`\`\n\`\`\`\nUse:\nNewLang helps in creating a new language category by making a new category, general chat, no-speaking text, and voice channel.\nOnce you've run the first command, that is !!newlang <name of language>, you will be asked for 3 inputs, namely the names of the three channels.\`\`\``
+  ),
+  async execute(msg, client, args) {
 
-      if (!args[0]) {
-        return msg.reply({embeds:[this.helpEmbed]}).then(msg => setTimeout(() => {
-            try {
-                msg.delete()
-            } catch (err) {
-              console.error(err)
-            }
-        }, 30000))
-      }
+    if (!args[0]) {
+      return msg.reply({embeds:[this.helpEmbed]}).then(msg => setTimeout(() => {
+          try {
+              msg.delete()
+          } catch (err) {
+            console.error(err)
+          }
+      }, 30000))
+    }
 
-    const category = client.channels.cache.find(
-      (channel) => channel.name.toLowerCase() === args[0].toLowerCase()
+    const category = await msg.guild.channels.cache.find(
+      (channel) => channel.name.toLowerCase() === args[0].toLowerCase() && channel.type === 'GUILD_CATEGORY'
     );
     if (!category)
       return msg.reply({
@@ -38,7 +38,7 @@ module.exports = {
       });
 
     const filter = (m) => m.author === msg.author && m.content === args[0];
-    reply = await msg.reply({
+    const reply = await msg.reply({
       content: `Please repeat the name of the language you want to delete. The same name as before. Incorrect messages will be ignored.`,
     });
 
@@ -62,5 +62,13 @@ module.exports = {
         collector.stop(`Successful`);
       }
     });
+    collector.on('end', async (collected, reason) => {
+      if (reason != `Successful`) {
+        reply.edit({content: `There was an error, please contact Matrical ASAP.`})
+        collected.delete()
+      } else {
+        reply.edit({content: `Language deletion of \`${args[0]}\` was successfully completed.`})
+      }
+    })
   }
 }
