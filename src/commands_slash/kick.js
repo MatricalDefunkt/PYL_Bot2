f56b7f3643ab1f450843157965e160b393a1e7e0
MatricalDefunkt <pramitsmusic@gmail.com>
2022-04-21 23:58:42 +0530

    Added kick/unban/ban slash commands

--- src/commands_slash/kick.js
+++ src/commands_slash/kick.js
@@ -5,16 +5,16 @@ const { rules } = require( '../rules.json' )
 module.exports = {
     data:
         new SlashCommandBuilder()
-            .setName( 'ban' )
-            .setDescription( 'Bans the given user.' )
+            .setName( 'kick' )
+            .setDescription( 'Kicks the given user.' )
             .addUserOption( o => o
                 .setName( 'user' )
-                .setDescription( 'User to ban' )
+                .setDescription( 'User to kick' )
                 .setRequired( true )
             )
             .addIntegerOption( o => o
                 .setName( 'reason' )
-                .setDescription( 'Reason for the ban (Will appear in Audit Logs)' )
+                .setDescription( 'Reason for the Kick (Will appear in Audit Logs)' )
                 .addChoices( [
                     [ 'Custom Reason', 0 ],
                     [ 'Gen Rule#1', 101 ],
@@ -35,38 +35,19 @@ module.exports = {
                 ] )
                 .setRequired( true )
             )
-            .addIntegerOption( o => o
-                .setName( 'msg-history' )
-                .setDescription( 'Delete the message history for the given time' )
-                .addChoices( [
-                    [ 'Don\'t delete', 0 ],
-                    [ '1 Day / 24 Hours', 1 ],
-                    [ '2 Days / 48 Hours', 2 ],
-                    [ '3 Days / 72 Hours', 3 ],
-                    [ '4 Days / 96 Hours', 4 ],
-                    [ '5 Days / 120 Hours', 5 ],
-                    [ '6 Days / 144 Hours', 6 ],
-                    [ '7 Days / 168 Hours', 7 ]
-                ] )
-                .setRequired( true )
-            )
-            .addBooleanOption( o => o
-                .setName( 'disputable' )
-                .setDescription( 'Whether the ban is disputable or not.' )
-            )
             .addStringOption( o => o
                 .setName( 'custom-reason' )
-                .setDescription( 'Please type the custom reason for the ban if you have chosen "Custom Reason" under \'Reason\'' )
+                .setDescription( 'Please type the custom reason for the kick if you have chosen "Custom Reason" under \'Reason\'' )
             ),
     helpEmbed: new MessageEmbed()
-        .setTitle( "Use of Ban" )
+        .setTitle( "Use of Kick" )
         .setAuthor( {
             name: "PYL Bot#9640",
             iconURL: `https://cdn.discordapp.com/avatars/954655539546173470/4c10aad2d82cdff4dcb05a6c83005739.webp`,
         } )
         .setColor( "GREEN" )
         .setDescription(
-            `Syntax and use of 'Ban' command:\n\`\`\`diff\n+   <Mandatory>\n-   [Optional]\`\`\`\n\`\`\`diff\n+   /ban <user> <reason> <days-of-messages-to-delete> [custom-reason] [disputable || Default: true]\`\`\`\n\`\`\`\nUse:\nThe ban command bans the provided user, sending them a DM with the reason, while logging the same reason in the audit logs. If applicable, the message will contain an invite to PYL Ban Appeals.\`\`\``
+            `Syntax and use of 'Kick' command:\n\`\`\`diff\n+   <Mandatory>\n-   [Optional]\`\`\`\n\`\`\`diff\n+   /kick <user> <reason> [custom-reason]\`\`\`\n\`\`\`\nUse:\nThe kick command kicks the provided user, sending them a DM with the reason, while logging the same reason in the audit logs.\`\`\``
         ),
     permissions: {
         ownerOnly: false,
@@ -77,16 +58,15 @@ module.exports = {
     {
 
         await interaction.deferReply( { ephemeral: true } );
-        const bannee = interaction.options.getMember( 'user' );
+        const kickee = interaction.options.getMember( 'user' );
         let _reason = interaction.options.getInteger( 'reason' );
-        const time = interaction.options.getInteger( 'msg-history' );
 
-        if ( bannee.bannable == false )
+        if ( kickee.kickable == false )
         {
-            return interaction.editReply( { content: `I cannot ban ${ bannee }. They're too powerful 🤯!!` } )
-        } else if ( interaction.member === bannee )
+            return interaction.editReply( { content: `I cannot kick ${ kickee }. They're too powerful 🤯!!` } )
+        } else if ( interaction.member === kickee )
         {
-            return interaction.editReply( { content: `Okay, you have been banned. Now move on.` } )
+            return interaction.editReply( { content: `Okay, you have been kicked. Now move on.` } )
         }
 
         const getRule = ( reasonID ) =>
@@ -112,44 +92,30 @@ module.exports = {
 
         }
 
-        const disputable = interaction.options.getBoolean( 'disputable' )
-        let disputableReply;
-
-        if ( disputable === false )
-        {
-            disputableReply = `Since this ban has been set as non-disputable, you may not dispute it, as it is now final.`
-        }
-        else 
-        {
-            disputableReply = `Since this ban has been set as disputable, you may join [this](https://discord.gg/UEwR4CUrug) server and dispute the ban there.`
-        }
-
         try
         {
-            const dmChannel = await bannee.createDM( true )
+            const dmChannel = await kickee.createDM( true )
             dmChannel.send( {
                 content: `Message from Practice Your Language:`, embeds: [
                     new MessageEmbed()
                         .setAuthor( { name: client.user.tag, iconURL: client.user.avatarURL( { size: 512 } ) } )
                         .setColor( 'RED' )
                         .setDescription( 'A message from PYL staff:' )
-                        .addField( 'Message:', 'You have been banned from PYL for breaking (a) server rule(s)' )
+                        .addField( 'Message:', 'You have been kicked from PYL for breaking (a) server rule(s)' )
                         .addField( 'Rule:', reason.rule )
-                        .addField( 'Dispute:', disputableReply )
-
                 ]
             } ).then( () =>
             {
-                interaction.editReply( { content: `${ bannee } has recieved the ban message.\nBanning now...` } )
-                bannee.ban( { days: time, reason: `${reason.reason}` } )
+                interaction.editReply( { content: `${ kickee } has recieved the kick message.\nKicking now...` } )
+                kickee.kick( reason.reason )
                     .then(
-                        interaction.editReply( { content: `${ bannee } has been banned.` } )
+                        interaction.editReply( { content: `${ kickee } has been kicked.` } )
                     )
                     .catch(
                         ( rejectedReason ) =>
                         {
                             interaction.editReply( { content: `Something went wrong. Please contact Matrical ASAP.` } )
-                            console.log(rejectedReason)
+                            console.log( rejectedReason )
                         } )
             } )
         } catch ( e )
@@ -157,8 +123,17 @@ module.exports = {
 
             if ( e.code === 50007 )
             {
-                await interaction.editReply( { content: `Cannot send messages to ${ bannee }\nBanning now...` } )
-                await bannee.ban( { days: time, reason: `${reason.reason}` } )
+                await interaction.editReply( { content: `Cannot send messages to ${ kickee }\nKicking now...` } )
+                await kickee.kick( reason.reason )
+                    .then(
+                        interaction.editReply( { content: `${ kickee } has been kicked, but did not recieve the message.` } )
+                    )
+                    .catch(
+                        ( rejectedReason ) =>
+                        {
+                            interaction.editReply( { content: `Something went wrong. Please contact Matrical ASAP.` } )
+                            console.log( rejectedReason )
+                        } )
             } else
             {
                 console.error( e )
