61ddceaee163ab18050d073ad20cfefb5452d487
MatricalDefunkt <pramitsmusic@gmail.com>
2022-04-21 23:57:37 +0530

    Brought deployL.js up to date, and fixed errors of .env

--- src/deployl.js
+++ src/deployl.js
@@ -1,45 +1,37 @@
-require("dotenv").config();
+require('dotenv').config();
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { token } = process.env.TOKEN;
+const token = process.env.TOKEN
 const fs = require('fs');
-const { guildId } = process.env.GUILDID;
-const { clientId } = process.env.CLIENTID;
+const guildId = process.env.GUILDID;
+const clientId = process.env.CLIENTID;
 
 const commands = [];
 const commandFiles = fs
-  .readdirSync("./commands")
+  .readdirSync("./commands_slash")
   .filter((file) => file.endsWith(".js"));
 const contextFiles = fs
-  .readdirSync("./contextmenus")
+  .readdirSync("./commands_contextmenu")
   .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
+  const command = require(`./commands_slash/${file}`);
   commands.push(command.data.toJSON());
-}
-
-for (const file of contextFiles) {
-  const contextMenu = require(`./contextmenus/${file}`);
-  commands.push(contextMenu.data.toJSON());
 }
 
 const rest = new REST({ version: "9" }).setToken(token);
 
-async () => {
-  try {
-    console.log(
-      "Started refreshing application (/) commands. (**LOCAL ONLY**)"
-    );
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands. (**LOCAL ONLY**)');
 
-    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-      body: commands,
-    });
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
 
-    console.log(
-      "Successfully reloaded application (/) commands. (**LOCAL ONLY**)"
-    );
-  } catch (error) {
-    console.error(error);
-  }
-};
+		console.log('Successfully reloaded application (/) commands. (**LOCAL ONLY**)');
+	} catch (error) {
+		console.error(error);
+	}
+})();
