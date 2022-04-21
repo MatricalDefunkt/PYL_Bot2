98486cb20a8278c26b82e6122e584b64fb23357a
MatricalDefunkt <pramitsmusic@gmail.com>
2022-04-21 23:58:23 +0530

    Added non-functioning TempBans table

--- src/database/database.js
+++ src/database/database.js
@@ -1,13 +1,13 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require( 'sequelize' )
 
-const sequelize  = new Sequelize('database', 'user', 'password', {
-	host: 'localhost',
-	dialect: 'sqlite',
-	logging: false,
-	storage: 'database.sqlite',
-});
+const sequelize = new Sequelize( 'database', 'user', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: false,
+    storage: 'database.sqlite',
+} );
 
-const Tags = sequelize.define('Tags', {
+const Tags = sequelize.define( 'Tags', {
     tagName: {
         type: Sequelize.STRING,
         unique: true,
@@ -18,6 +18,33 @@ const Tags = sequelize.define('Tags', {
     tagAuthor: Sequelize.STRING,
 }, {
     tableName: 'Tags',
-});
+} );
 
-module.exports = { Tags }
+const tempBans = sequelize.define( 'tempBans', {
+    userID: {
+        type: Sequelize.TEXT,
+        unique: true,
+        primaryKey: true
+    },
+    finishTimeStamp: Sequelize.INTEGER,
+    modID: Sequelize.TEXT,
+    reason: Sequelize.STRING,
+}, {
+    tableName: 'tempBans',
+} );
+try
+{
+    const sync = async ( Tags, tempBans ) =>
+    {
+        await Tags.sync()
+        await tempBans.sync()
+    }
+    sync( Tags, tempBans )
+} catch ( e )
+{
+    console.error( e );
+}
+//TODO: Create table for infractions
+//TODO: Work on it lol
+
+module.exports = { Tags, tempBans }
