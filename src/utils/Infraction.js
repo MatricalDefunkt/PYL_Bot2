95e854497e198c6e8a1d6fa5a67502b829298e43
MatricalDefunkt <pramitsmusic@gmail.com>
2022-05-03 02:40:48 +0530

    Accomoted for convert ban type infractions

--- src/utils/Infraction.js
+++ src/utils/Infraction.js
@@ -13,6 +13,7 @@ class Infraction
         this.mute;
         this.tempMute;
         this.warn;
+        this.convertBan;
     }
     /**
      * Adds a note in the database for the given user.
@@ -49,6 +50,60 @@ class Infraction
             reason: reason
         } ).catch( error => console.log( error ) )
         return this.ban = ban
+    }
+    /**
+     * Adds an unban in the database for the given user.
+     * @param {String} moderatorId 
+     * @param {String} target 
+     * @param {String} reason 
+     * @returns {Promise<Model> | Error}
+     */
+    async addUnBan ( moderatorId, target, reason )
+    {   
+        const unban = await Infractions.create( {
+            caseID: uniqid( 'unban--' ),
+            type: 'Unban',
+            targetID: target,
+            modID: moderatorId,
+            reason: reason
+        } ).catch( error => console.log( error ) )
+        return this.unban = unban
+    }
+    /**
+     * Adds a temporary ban in the database for the given user.
+     * @param {String} moderatorId 
+     * @param {String} target 
+     * @param {String} reason 
+     * @returns {Promise<Model> | Error}
+     */
+    async addTempBan ( moderatorId, target, reason )
+    {   
+        const tempBan = await Infractions.create( {
+            caseID: uniqid( 'tempban--' ),
+            type: 'TempBan',
+            targetID: target,
+            modID: moderatorId,
+            reason: reason
+        } ).catch( error => console.log( error ) )
+        return this.tempBan = tempBan
+    }
+    /**
+     * Adds a converted ban in the database for the given user.
+     * @param {String} moderatorId 
+     * @param {String} target 
+     * @param {String} reason 
+     * @returns {Promise<Model> | Error}
+     */
+    async addConvertBan ( moderatorId, target, reason )
+    {   
+        const convertBan = await Infractions.create( {
+            caseID: uniqid( 'tempban--' ),
+            type: 'TempBan',
+            targetID: target,
+            modID: moderatorId,
+            reason: reason
+        } ).catch( error => console.log( error ) )
+        return this.convertBan = convertBan
     }
 }
 
