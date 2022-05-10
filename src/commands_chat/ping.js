b7542b1a0f58635f2adbb162797e31ee2868857b
MatricalDefunkt <pramitsmusic@gmail.com>
2022-05-10 17:31:43 +0530

    Added check for WS ping

--- src/commands_chat/ping.js
+++ src/commands_chat/ping.js
@@ -1,9 +1,11 @@
+const { Client, Message } = require( "discord.js" );
+
 module.exports = {
   data: {
     name: "ping",
   },
   help: {
-    helpDescription: `The ping command replies with the roundtrip latency of the bot. Meaning, the time it takes for a bot to recieve a message subtracted from the time it takes to send a message, from, and to Discord.`,
+    helpDescription: `The ping command replies with the roundtrip latency of the , as well as the websocket ping, meaning, the time it takes for a bot to recieve a message subtracted from the time it takes to send a message, from, and to Discord.`,
     helpSyntax: `ping`,
     helpEmbed: false
   },
@@ -12,12 +14,17 @@ module.exports = {
     staffOnly: false,
     adminOnly: false,
   },
+  /**
+   * 
+   * @param {Message} msg 
+   * @param {Client} client 
+   * @param {Array<String>} args 
+   */
   async execute ( msg, client, args )
   {
     const sent = await msg.reply( { content: "Pinging..." } );
     sent.edit(
-      `Roundtrip latency is ${ sent.createdTimestamp - msg.createdTimestamp
-      }ms.>`
+      `Roundtrip Latency: ${ sent.createdTimestamp - msg.createdTimestamp}ms.\nWebsocket Ping: ${ client.ws.ping }ms.`
     );
   }
 }
