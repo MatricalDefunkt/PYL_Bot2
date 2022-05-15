cc5f5756f6ecb7225ec8e564f150f38c974d92ec
MatricalDefunkt <pramitsmusic@gmail.com>
2022-05-16 04:14:59 +0530

    Accomodated for unmutes and kicks

--- src/commands_slash/commands_slash_sub/logs/count.js
+++ src/commands_slash/commands_slash_sub/logs/count.js
@@ -13,7 +13,7 @@ module.exports = {
      */
     async execute ( client, interaction )
     {
-        const target = interaction.options.getMember( 'target' );
+        const target = interaction.options.getUser( 'target' );
 
         const note = await Infractions.count( { where: { targetID: target.id, type: 'Note' } } )
         const ban = await Infractions.count( { where: { targetID: target.id, type: 'Ban' } } )
@@ -22,14 +22,20 @@ module.exports = {
         const mute = await Infractions.count( { where: { targetID: target.id, type: 'Mute' } } )
         const tempMute = await Infractions.count( { where: { targetID: target.id, type: 'TempMute' } } )
         const warn = await Infractions.count( { where: { targetID: target.id, type: 'Warn' } } )
+        const kick = await Infractions.count( { where: { targetID: target.id, type: 'Kick' } } )
         const unban = await Infractions.count( { where: { targetID: target.id, type: 'Unban' } } )
+        const unmute = await Infractions.count( { where: { targetID: tagrget.id, type: 'UnMute' } } )
 
-        const messageBuilder = [ { name: 'Notes:', value: `${ note }`, inline: true }, { name: 'Warns:', value: `${ warn }`, inline: true }, { name: 'Temp Mutes:', value: `${ tempMute }`, inline: true }, { name: 'Mutes:', value: `${ mute }`, inline: true }, { name: 'Temp Bans:', value: `${ tempBan }`, inline: true }, { name: 'Converted Bans:', value: `${ unban }`, inline: convertBan }, { name: 'Bans:', value: `${ ban }`, inline: true }, { name: 'Unbans:', value: `${ unban }`, inline: true } ]
+        const messageBuilder = [
+            { name: 'Notes:', value: `${ note }`, inline: true }, { name: 'Warns:', value: `${ warn }`, inline: true }, { name: 'Temp Mutes:', value: `${ tempMute }`, inline: true },
+            { name: 'Mutes:', value: `${ mute }`, inline: true }, { name: 'Kicks', value: `${ kick }`, inline: true }, { name: 'Temp Bans:', value: `${ tempBan }`, inline: true },
+            { name: 'Converted Bans:', value: `${ unban }`, inline: true }, { name: 'Bans:', value: `${ ban }`, inline: true }, { name: 'Unmutes', value: `${ unmute }`, inline: true },
+            { name: 'Unbans:', value: `${ unban }`, inline: true } ]
 
         const embed = new MessageEmbed()
             .setAuthor( { name: interaction.user.tag, iconURL: interaction.user.avatarURL() } )
             .addFields( messageBuilder )
-            .setColor( 'YELLOW' )
+            .setColor( ( ban > 0 ) ? 'RED' : 'YELLOW' )
 
         interaction.editReply( { embeds: [ embed ] } )
 
